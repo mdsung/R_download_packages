fd6ab5f025b944a7a192c3f949db604db45fa71e
mdsung <mdskylover@gmail.com>
2021-08-17 11:01:32 +0900

    revised download library script

--- code/download_github_libraries_with_dependency.R
+++ code/download_github_libraries_with_dependency.R
@@ -4,17 +4,9 @@
 ## Objective: github download with dependencies
 
 library(pkgdepends)
-library(here)
 
-## One file example
-# package_name <- 'Achilles'    
-# pdl <- new_pkg_download_proposal(glue("OHDSI/{package_name}"), config = list(cache_dir = target_dir))
-# pdl$resolve()
-# pdl$download()
-# gd <- pdl$get_downloads()
-
-## List of archilles packages
-# Achilles-Node is not a package, just a R code
+# Constants
+package_download_dir <- "/libraries"
 cran_packages <- c('devtools', 
                     'glue', 
                     'here', 
@@ -30,20 +22,15 @@ ohdsi_github_packages <- c("Achilles",
                            "OhdsiRTools",
                            "OhdsiSharing",
                            "SqlRender")
-## Functionalize
-# github_downloader <- function(package_name){
-#     utils::download.file(glue("https://api.github.com/repos/OHDSI/{package_name}/tarball"), 
-#                          destfile = here(glue("libraries/{package_name}.tar.gz")))
-# }
 
-## download dependencies from tar.gz file
+# download dependencies from tar.gz file
 download_dependencies <- function(package_name, target_dir){
     print(package_name)
     pdl <- new_pkg_download_proposal(package_name, config = list(cache_dir = target_dir))
     pdl$resolve()
     pdl$download()
     if (package_name == 'pkgdepends'){
-        write.csv(pdl$get_downloads(), 'pkgdepends_dependencies.csv')
+        print(pdl$get_downloads())
     }
 }
 
@@ -56,9 +43,9 @@ download_ohdsi_dependencies <- function(package_name, target_dir){
 
 ## execute
 for (package in cran_packages){
-    download_dependencies(package, here("libraries"))
+    download_dependencies(package, package_download_dir)
 }
 for (package in ohdsi_github_packages){
-    download_ohdsi_dependencies(package, here("libraries"))
+    download_ohdsi_dependencies(package, package_download_dir)
 }
 
--- dockerfile
+++ dockerfile
@@ -8,10 +8,10 @@ RUN apt-get update \
     curl \
     libcurl4-openssl-dev \
     && rm -rf /var/lib/apt/lists/*
+RUN R -e "install.packages('pkgdepends')" && mkdir R_download_packages
 
-RUN git clone https://github.com/mdsung/R_download_packages && cd R_download_packages
-RUN R -e "install.packages(c('renv', 'here', 'pkgdepends'))" 
+COPY code/download_github_libraries_with_dependency.R /R_download_packages
 
 WORKDIR /R_download_packages
 
-CMD ["Rscript", "code/download_github_libraries_with_dependency.R"]
+CMD ["Rscript", "download_github_libraries_with_dependency.R"]
